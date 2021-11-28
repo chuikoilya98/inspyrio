6a55cf900fd1cace5b94a3129d5be846df98ce93
chuikoilya98 <chuikoilya98@gmail.com>
2021-11-28 18:21:11 +0500

    getting info

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ from io import BytesIO
 app = Flask(__name__)
 
 @app.route('/')
-def index() :
-    return 'webhook test'
+def receive_update():
+    if request.method == "POST":
+        print(request.json)
+    return {"ok": True}
 
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
+
