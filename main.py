81ffb6eefd8aa8d71282f99d2ffe348a76bbba13
chuikoilya98 <chuikoilya98@gmail.com>
2021-11-28 18:07:21 +0500

    wh test

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-import requests
+from flask import Flask
+from flask import request
+from flask import send_file, send_from_directory
+import os.path as pt
+import os
+from io import BytesIO
+
+app = Flask(__name__)
+
+@app.route('/')
+def index() :
+    return 'webhook test'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
